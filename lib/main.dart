15357ccb5bd472db43ce35f05336b6e785509f12
Haidar <afiq.haidar12@gmail.com>
2024-05-08 10:16:30 +0700

    feat: homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/start_screen.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 255, 0, 0),
+                Color.fromARGB(255, 255, 85, 85),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/quiz-logo.png',
+            width: 300,
+            color: Color.fromARGB(135, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter The Fun Way',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.all(10),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
